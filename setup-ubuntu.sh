3f248350b0f595f0b61a5b5f4039187a30a92c6f
hyoungseok <hyoungseok.k@gmail.com>
2020-11-09 00:50:26 +0900

    [Bash] setup script

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -9,4 +9,4 @@ echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nf
 pyenv install miniconda3-4.7.10
 pyenv global miniconda3-4.7.10
 conda install -y pytorch==1.3.1
-pip install -r ~/multicampus-NLP/requirements.txt 
+pip install -r ~/multicampus-NLP/requirements-ubuntu.txt 
