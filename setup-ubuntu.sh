345ad0147456a38a0d64219051666da1de2fd95b
hyoungseok <hyoungseok.k@gmail.com>
2020-11-09 00:53:44 +0900

    [Bash] setup script

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd
-sudo apt install -y git
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+source .profile
 pyenv install miniconda3-4.7.10
 pyenv global miniconda3-4.7.10
 conda install -y pytorch==1.3.1
