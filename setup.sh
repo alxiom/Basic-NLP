466c4ddc9468dd0ab223abd323074af6061e8eae
Alex <hyoungseok.k@gmail.com>
2020-11-09 00:39:46 +0900

    [Bash] install library

--- setup.sh
+++ setup.sh
@@ -6,3 +6,7 @@ git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+pyenv install miniconda3-4.7.10
+pyenv global miniconda3-4.7.10
+conda install -y pytorch==1.3.1
+pip install -r ~/multicampus-NLP/requirements.txt 
